247cd8f103f47371cde0190d5bc8ef7d8df9088c
Parth3053 <nirmalvora61@gmail.com>
2022-03-14 09:16:59 +0530

    marge to main

--- lib/navigation/navigation_config.dart
+++ lib/navigation/navigation_config.dart
@@ -3,12 +3,15 @@ import 'package:triya_app/navigation/navigation_constant.dart';
 import 'package:triya_app/ui/auth/candidate_login/candidate_choose_login_type.dart';
 import 'package:triya_app/ui/auth/deshboard/home/resume/academic_history_screen.dart';
 import 'package:triya_app/ui/auth/deshboard/home/resume/contact_information_screen.dart';
-import 'package:triya_app/ui/auth/deshboard/home/resume/proffessional_screen.dart';
+import 'package:triya_app/ui/auth/deshboard/home/resume/froffessional_screen.dart';
 import 'package:triya_app/ui/auth/deshboard/home/resume/key_skills_screen.dart';
 import 'package:triya_app/ui/auth/deshboard/home/resume/normal_resume.dart';
 import 'package:triya_app/ui/auth/deshboard/home/resume/personal_statement_screen.dart';
 import 'package:triya_app/ui/auth/deshboard/home/resume/resume.dart';
+import 'package:triya_app/ui/auth/employer_dashboard/employer_dashboard.dart';
 import 'package:triya_app/ui/auth/employer_login/employer_login_screen.dart';
+import 'package:triya_app/ui/auth/employer_login/employer_sign_in.dart';
+import 'package:triya_app/ui/auth/employer_login/employer_sign_up.dart';
 import 'package:triya_app/ui/auth/login/login_screen.dart';
 import 'package:triya_app/ui/auth/login_type_screen.dart';
 import 'package:triya_app/ui/auth/splash_screen.dart';
@@ -19,10 +22,6 @@ import 'package:triya_app/ui/deshboard/home/gov/gov_description.dart';
 import 'package:triya_app/ui/deshboard/home/home.dart';
 import 'package:triya_app/ui/deshboard/home/video_category.dart';
 import 'package:triya_app/ui/deshboard/home/videos.dart';
-
-import '../ui/auth/employer_dashboard/employer_dashboard.dart';
-import '../ui/auth/employer_login/employer_sign_in.dart';
-import '../ui/auth/employer_login/employer_sign_up.dart';
 
 class NavigationRoute {
   static final List<GetPage> pages = [
