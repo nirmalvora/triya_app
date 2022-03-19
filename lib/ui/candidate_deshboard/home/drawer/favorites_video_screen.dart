0a901049484b51631132f968de9dae3571c5f302
Parth3053 <nirmalvora61@gmail.com>
2022-03-19 13:00:49 +0530

    favorites video create

---
+++ lib/ui/candidate_deshboard/home/drawer/favorites_video_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:triya_app/constants/color_constant.dart';
+import 'package:triya_app/constants/image_constant.dart';
+import 'package:triya_app/utils/app_utils.dart';
+
+class FavoritesVideoScreen extends StatelessWidget {
+  const FavoritesVideoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorConstant.jobBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: ColorConstant.backgroundColor,
+        elevation: 0,
+        leading: InkWell(
+          onTap: () {
+            Get.back();
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: CircleAvatar(
+              backgroundColor: ColorConstant.droverButtonColor,
+              radius: 18,
+              child: Icon(Icons.arrow_back),
+            ),
+          ),
+        ),
+        title: Text(
+          "Favorites Books",
+          style: TextStyle(color: ColorConstant.splashColor),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 15),
+            child: CircleAvatar(
+              radius: 22,
+              child: Image.asset(
+                AppUtils.getPNGAsset(ImageConstant.myProfileIcon),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
